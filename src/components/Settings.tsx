904c2551f684ba58fb8a31db221d401d3c2d3622
Armani Ferrante <armaniferrante@gmail.com>
2021-06-05 09:41:52 -0700

    Disable auto close button (#32)

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -192,7 +192,7 @@ function CloseNewAccountsSwitch() {
   const { isClosingNewAccounts, setIsClosingNewAccounts } = useSwapContext();
 
   return (
-    <FormGroup row>
+    <FormGroup style={{ display: "none" }} row>
       <FormControlLabel
         classes={{ label: styles.closeAccountSwitchLabel }}
         labelPlacement="start"
